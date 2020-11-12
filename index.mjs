677be7d918d0a97561fec56cb90e26c1257e43aa
Dieter Verschueren <dieterverschueren@gmail.com>
2020-11-12 12:09:32 +0100

    fixes & improvements

--- index.mjs
+++ index.mjs
@@ -16,38 +16,43 @@ let latestEventId;
 		token: config.TELEGRAM_TOKEN
 	});
 
-	// open the database
-	const db = await open({
+	const dbConfig = {
 		filename: `db/${config.EVENT_DB}`,
-		driver: sqlite3.Database,
-		mode: sqlite3.OPEN_READONLY
-	});
+		driver: sqlite3.Database
+	};
+
+	// open the database and fetch latest event id on startup
+	const db = await open(dbConfig);
 
 	const { id } = await db.get(`SELECT max(event_id) AS id FROM ${config.EVENT_TABLE} ORDER BY event_id DESC;`);
 	logger.info(`Most recent event id is ${id}`);
 	latestEventId = id;
+	await db.close();
 
-	setInterval(() => processEventLog(db, telegram), config.EVENT_INTERVAL);
+	// start checking process
+	setInterval(() => processEventLog(dbConfig, telegram), config.EVENT_INTERVAL);
 })();
 
-async function processEventLog(db, telegram) {
+async function processEventLog(dbConfig, telegram) {
 	logger.debug('Checking for new events');
-	const results = await db.all(`SELECT * FROM ${config.EVENT_TABLE} WHERE event_id > ${latestEventId} ORDER BY event_id ASC;`);
+	const db = await open(dbConfig);
+
+	// limit events per 5 to avoid too many requests on the telegram bot api
+	const results = await db.all(`SELECT * FROM ${config.EVENT_TABLE} WHERE event_id > ${latestEventId} ORDER BY event_id ASC LIMIT 5;`);
 
 	for (const { event_id: id, event_date: date, event_time: time, event_desc: desc } of results) {
 		try {
 			await telegram.sendMessage({
 				chat_id: config.TELEGRAM_CHAT_ID,
-				text: `QNAP System event at ${date.replace(/-/g, '\\-')} ${time}:\\\n\\\n\`\`\`${desc}\`\`\``,
-				parse_mode: 'MarkdownV2',
-				disable_notification: desc.includes('Failed to upload file')
+				text: `QNAP System event at ${date} <b>${time}</b>:\n\n<pre>${desc.replace(/<->/g, '⟷').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;')}</pre>`,
+				parse_mode: 'HTML',
+				disable_notification: `${desc.includes('Failed to upload file')}`
 			});
 
 			logger.info(`Processed new event_id ${id}`);
 			latestEventId = id;
 		} catch (error) {
-			logger.error(error);
+			logger.error(`Message not send for event_id ${id}`, error);
 		}
-
 	}
 }
